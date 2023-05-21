74b2221cda08e83be0337318bb82ab1e19c1e1a4
vinay <54681400+sudovinay01@users.noreply.github.com>
2023-05-21 16:07:45 +0530

    Create _03_Catalogs.java

---
+++ learn-JDBC/src/_06_metadata/_01_getMetaData/_03_Catalogs.java
@@ -0,0 +1,36 @@
+package _06_metadata._01_getMetaData;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class _03_Catalogs {
+
+	public static void main(String[] args) throws SQLException {
+		System.out.println(_03_Catalogs.class.getName());
+		System.out.println("Description : Using getMetaData to get catalogs.");
+		System.out.println("Connenecting to Database..");	
+		try (Connection connection = _01_connection._01_DriverManagerFullURI.getConnection()) {
+			System.out.println("Connection established? " + connection.isValid(0));
+			printDriverDetails(connection);
+		}
+	}
+
+	public static void printDriverDetails(Connection connection) throws SQLException {
+		DatabaseMetaData dbmd = connection.getMetaData();
+		ResultSet rs = dbmd.getCatalogs();
+		System.out.println("====================================");
+		System.out.println("getCatalogs");
+		System.out.println("====================================");
+		while (rs.next()) {
+			System.out.println(rs.getString(1));
+		}
+		System.out.println("====================================");
+		System.out.println("getCatalogSeparator() => "+dbmd.getCatalogSeparator());
+		System.out.println("getCatalogTerm() => "+dbmd.getCatalogTerm());
+		System.out.println("getMaxCatalogNameLength() => "+dbmd.getMaxCatalogNameLength());
+		System.out.println("isCatalogAtStart() => "+dbmd.isCatalogAtStart());
+	}
+
+}
