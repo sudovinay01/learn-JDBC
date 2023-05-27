252cba7b2f14c998918c2f179cc0cb9cc81f6fe6
vinay <54681400+sudovinay01@users.noreply.github.com>
2023-05-27 16:56:25 +0530

    Create _08_Schemas.java

---
+++ learn-JDBC/src/_06_metadata/_01_DatabaseMetaDataa/_08_Schemas.java
@@ -0,0 +1,38 @@
+package _06_metadata._01_DatabaseMetaDataa;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class _08_Schemas {
+
+	public static void main(String[] args) throws SQLException {
+		System.out.println(_08_Schemas.class.getName());
+		System.out.println("Description : Using getMetaData to get details about Schemass.");
+		System.out.println("Connenecting to Database..");	
+		try (Connection connection = _01_connection._01_DriverManagerFullURI.getConnection()) {
+			System.out.println("Connection established? " + connection.isValid(0));
+			printCatalogDetails(connection);
+		}
+	}
+
+	public static void printCatalogDetails(Connection connection) throws SQLException {
+		DatabaseMetaData dbmd = connection.getMetaData();
+		ResultSet rs = dbmd.getSchemas();
+		System.out.println("====================================");
+		System.out.println("getSchemas");
+		System.out.println("====================================");
+		_00_data.ResultSetUtils.printResultSet(rs);
+		System.out.println("====================================");
+		System.out.println("getSchemaTerm() => "+dbmd.getSchemaTerm());
+		System.out.println("getMaxSchemaNameLength() => "+dbmd.getMaxSchemaNameLength());
+		rs = dbmd.getSchemas(null, null);
+		System.out.println("====================================");
+		System.out.println("getSchemas");
+		System.out.println("====================================");
+		_00_data.ResultSetUtils.printResultSet(rs);
+		System.out.println("====================================");
+	}
+
+}
