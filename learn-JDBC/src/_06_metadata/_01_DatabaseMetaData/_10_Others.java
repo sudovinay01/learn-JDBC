8f35a61a81fc2a93996839445b9ee4af1e01c9cd
vinay <54681400+sudovinay01@users.noreply.github.com>
2023-05-28 16:22:10 +0530

    added _01_DatabaseMetaData\_11_keys.java

--- learn-JDBC/src/_06_metadata/_01_DatabaseMetaData/_10_Others.java
+++ learn-JDBC/src/_06_metadata/_01_DatabaseMetaData/_10_Others.java
@@ -1,21 +1,16 @@
 package _06_metadata._01_DatabaseMetaData;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Types;
-import java.util.ArrayList;
-import java.util.List;
 
 public class _10_Others {
 
 	public static void main(String[] args)
 			throws SQLException, IllegalArgumentException, IllegalAccessException, SecurityException {
 		System.out.println(_10_Others.class.getName());
-		System.out.println("Description : Using getMetaData to get supported operations.");
+		System.out.println("Description : Using getMetaData to get values.");
 		System.out.println("Connenecting to Database..");
 		try (Connection connection = _01_connection._01_DriverManagerFullURI.getConnection()) {
 			System.out.println("Connection established? " + connection.isValid(0));
