3d105489a1f7dbec6fc2993b94e351486913759f
vinay <54681400+sudovinay01@users.noreply.github.com>
2023-05-28 13:58:02 +0530

    completed _09_Support.java

--- learn-JDBC/src/_06_metadata/_01_DatabaseMetaData/_09_Support.java
+++ learn-JDBC/src/_06_metadata/_01_DatabaseMetaData/_09_Support.java
@@ -1,8 +1,10 @@
 package _06_metadata._01_DatabaseMetaData;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.ArrayList;
@@ -10,7 +12,8 @@ import java.util.List;
 
 public class _09_Support {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args)
+			throws SQLException, IllegalArgumentException, IllegalAccessException, SecurityException {
 		System.out.println(_09_Support.class.getName());
 		System.out.println("Description : Using getMetaData to get supported operations.");
 		System.out.println("Connenecting to Database..");
@@ -20,7 +23,8 @@ public class _09_Support {
 		}
 	}
 
-	public static void printSupportedOerationsDetails(Connection connection) throws SQLException {
+	public static void printSupportedOerationsDetails(Connection connection)
+			throws SQLException, IllegalArgumentException, IllegalAccessException, SecurityException {
 		DatabaseMetaData dbmd = connection.getMetaData();
 		List<Method> supportMethods = new ArrayList<>();
 
@@ -41,24 +45,60 @@ public class _09_Support {
 				if (supportMethod.getReturnType() != void.class && supportMethod.getParameterCount() == 0) {
 					// Invoke the method and capture the return value
 					Object result = supportMethod.invoke(dbmd);
-//					System.out.println(supportMethod.getName() + "() => " + result);
-				} else {
-					System.out.println("==> " + supportMethod);
+					System.out.println(supportMethod.getName() + "() => " + result);
 				}
 			} catch (Exception e) {
-				System.out.println("==> " + supportMethod);
 				e.printStackTrace();
 			}
 		}
-		System.out.println("supportsTransactionIsolationLevel(Connection.TRANSACTION_NONE) => " + dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_NONE));
-		System.out.println("supportsTransactionIsolationLevel(Connection.TRANSACTION_READ_COMMITTED) => " + dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_READ_COMMITTED));
-		System.out.println("supportsTransactionIsolationLevel(Connection.TRANSACTION_READ_UNCOMMITTED) => " + dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_READ_UNCOMMITTED));
-		System.out.println("supportsTransactionIsolationLevel(Connection.REPEATABLE_READ) => " + dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_REPEATABLE_READ));
-		System.out.println("supportsTransactionIsolationLevel(Connection.TRANSACTION_SERIALIZABLE) => " + dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_SERIALIZABLE));
 
-		System.out.println("supportsConvert(Types.INTEGER, Types.VARCHAR) => " + dbmd.supportsConvert(Types.INTEGER, Types.VARCHAR));
-		System.out.println("supportsConvert(Types.DATE, Types.INTEGER) => " + dbmd.supportsConvert(Types.DATE, Types.INTEGER));
+		System.out.println("supportsTransactionIsolationLevel(Connection.TRANSACTION_NONE) => "
+				+ dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_NONE));
+		System.out.println("supportsTransactionIsolationLevel(Connection.TRANSACTION_READ_COMMITTED) => "
+				+ dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_READ_COMMITTED));
+		System.out.println("supportsTransactionIsolationLevel(Connection.TRANSACTION_READ_UNCOMMITTED) => "
+				+ dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_READ_UNCOMMITTED));
+		System.out.println("supportsTransactionIsolationLevel(Connection.REPEATABLE_READ) => "
+				+ dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_REPEATABLE_READ));
+		System.out.println("supportsTransactionIsolationLevel(Connection.TRANSACTION_SERIALIZABLE) => "
+				+ dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_SERIALIZABLE));
 
+		System.out.println("supportsResultSetHoldability(ResultSet.HOLD_CURSORS_OVER_COMMIT) => "
+				+ dbmd.supportsResultSetHoldability(ResultSet.HOLD_CURSORS_OVER_COMMIT));
+		System.out.println("supportsResultSetHoldability(ResultSet.CLOSE_CURSORS_AT_COMMIT) => "
+				+ dbmd.supportsResultSetHoldability(ResultSet.CLOSE_CURSORS_AT_COMMIT));
+
+		System.out.println("===============================\n" + "Supported ResultSet types\n"
+				+ "===============================");
+		for (Field type : ResultSet.class.getFields()) {
+			if (type.getName().startsWith("TYPE")) {
+				System.out.println("supportsResultSetType(" + type.getName() + ") => "
+						+ dbmd.supportsResultSetType(type.getInt(type)));
+			}
+		}
+
+		System.out.println("===============================\n" + "Supported ResultSet Concurrency\n"
+				+ "===============================");
+		for (Field type : ResultSet.class.getFields()) {
+			if (type.getName().startsWith("TYPE")) {
+				for (Field consurrency : ResultSet.class.getFields()) {
+					if (consurrency.getName().startsWith("CONCUR")) {
+						System.out.println("supportsResultSetConcurrency(" + type.getName() + ", "
+								+ consurrency.getName() + ") => " + dbmd.supportsResultSetConcurrency(type.getInt(type),
+										consurrency.getInt(consurrency)));
+					}
+				}
+			}
+		}
+
+		System.out.println(
+				"==========================\n" + "Supported type conversions\n" + "==========================");
+		for (Field fromType : Types.class.getFields()) {
+			for (Field toType : Types.class.getFields()) {
+				System.out.println("supportsConvert(" + fromType.getName() + "," + toType.getName() + ") => "
+						+ dbmd.supportsConvert(fromType.getInt(fromType), toType.getInt(toType)));
+			}
+		}
 	}
 
 }
